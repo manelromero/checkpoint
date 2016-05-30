4c4b64c71017e2c5e96fa482a9074750d5247258
Manel Romero <manel@manelromero.com>
2016-05-30 17:43:51 +0200

    Changes for testing

--- app/__init__.py
+++ app/__init__.py
@@ -17,10 +17,10 @@ app = Flask(__name__)
 app.config.from_object('config')
 
 # config file por production
-app.instance_path = os.path.abspath(os.path.join(os.path.dirname(__file__), \
-	'..'))
-config_file_path = app.instance_path + '/instance/config.py'
-app.config.from_pyfile(config_file_path)
+# app.instance_path = os.path.abspath(os.path.join(os.path.dirname(__file__), \
+# 	'..'))
+# config_file_path = app.instance_path + '/instance/config.py'
+# app.config.from_pyfile(config_file_path)
 
 
 import views
--- config.py
+++ config.py
@@ -1,2 +1,6 @@
-DEBUG = False
-VERIFY = True
+DEBUG = True
+VERIFY = False
+SECRET_KEY = 'secret_key'
+SERVER = '192.168.1.10'
+PORT = '443'
+LAYER = 'Network'
